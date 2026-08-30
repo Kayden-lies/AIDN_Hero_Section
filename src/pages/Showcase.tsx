11d65e213ccd70c1768dccfc5276044abdbeaeb0
Kayden-lies <sondo0963@gmail.com>
2026-08-30 23:21:40 +0530

    Create Showcase.tsx

---
+++ src/pages/Showcase.tsx
@@ -0,0 +1,11 @@
+import ShowcaseHeader from '../components/showcase/ShowcaseHeader';
+import ShowcaseForm from '../components/showcase/ShowcaseForm';
+
+export default function Showcase() {
+  return (
+    <main className="showcase-page">
+      <ShowcaseHeader />
+      <ShowcaseForm />
+    </main>
+  );
+}
